8a677ddfca0c0c7f5c0f221d4658a14e790c0613
burak <110386342+burakeraslan@users.noreply.github.com>
2023-10-03 20:00:36 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_webscraping/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -20,6 +26,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // CollectionReference deneme = FirebaseFirestore.instance.collection('deneme');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
